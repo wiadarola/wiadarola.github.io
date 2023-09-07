05df42aae46fe9d98334fbd4e550f3b3171b4f69
William Iadarola <wiadarola0@gmail.com>
2023-09-06 18:59:53 -0700

    Re-added skills; Skills templated slider cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import NavTop from './components/NavTop';
 import Hero from './components/Hero';
-// import Skills from './components/Skills';
+import Skills from './components/Skills';
 // import Projects from './components/Projects';
 // import Description from './components/Description';
 // import About from './components/About';
@@ -14,11 +14,11 @@ function App() {
       < NavBar />
       < NavTop />
       < Hero />
-      {/* < Skills />
-      < Projects />
-      < Description />
-      < About />
-      < Contact /> */}
+      < Skills />
+      {/* < Projects /> */}
+      {/* < Description /> */}
+      {/* < About /> */}
+      {/* < Contact /> */}
     </>
   );
 }
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,26 +1,74 @@
 import './index.css';
 import Divider from '../Divider';
+import { useState } from 'react';
 
-function Skills() {
-    const languages = ['JavaScript / TypeScript', 'HTML / CSS', 'C / C++', 'Python', 'Java', 'PHP', 'SQL'];
-    const frameworks = ['Flask', 'React', 'JQuery', 'MySQL', 'Bootstrap', 'Oracle Database'];
+export default function Skills() {
+    const [selectedSkillType, setSelectedSkillType] = useState('language');
+
+    const skillsData = [
+        { name: 'JavaScript', type: 'language', description: 'Used in several frontend projects.' },
+        { name: 'TypeScript', type: 'language', description: 'Applied in React and Angular projects.' },
+        { name: 'HTML', type: 'language', description: 'Markup language for web development.' },
+        { name: 'CSS', type: 'language', description: 'Styling for web applications.' },
+        { name: 'C', type: 'language', description: 'Low-level programming for system development.' },
+        { name: 'C++', type: 'language', description: 'Object-oriented, used in various applications.' },
+        { name: 'Assembly', type: 'language', description: 'Lowest level human-readable language.' },
+        { name: 'Python', type: 'language', description: 'Versatile language for web, data science, and more.' },
+        { name: 'Java', type: 'language', description: 'Object-oriented, used in various applications.' },
+        { name: 'PHP', type: 'language', description: 'Server-side scripting for web development.' },
+        { name: 'SQL', type: 'language', description: 'Used for database queries.' },
+        { name: 'Flask', type: 'framework', description: 'Python micro web framework.' },
+        { name: 'React', type: 'framework', description: 'JavaScript library for building user interfaces.' },
+        { name: 'JQuery', type: 'framework', description: 'JavaScript library for DOM manipulation.' },
+        { name: 'MySQL', type: 'framework', description: 'Open-source relational database management system.' },
+        { name: 'Bootstrap', type: 'framework', description: 'CSS framework for responsive design.' },
+        { name: 'Oracle Database', type: 'framework', description: 'Object-relational database management system.' },
+        { name: 'Node.js', type: 'tool', description: 'Runtime for executing JavaScript on the server side.' },
+        { name: 'Vue.js', type: 'framework', description: 'Progressive framework for building user interfaces.' },
+        { name: 'Docker', type: 'tool', description: 'Platform for containerization.' },
+        { name: 'Kubernetes', type: 'tool', description: 'Container orchestration system.' },
+        { name: 'Git', type: 'tool', description: 'Version control system.' },
+        { name: 'PostgreSQL', type: 'framework', description: 'Open-source relational database.' }
+    ];
 
     return (
-        < Divider content={
-            <>
-                <h1 className='header'>SKILLS</h1>
-                <div id="skills-container">
-                    <div id='skills-languages'>
-                        {languages.map((language) => { return <div key={language} className='skills-language'>{language}</div>; })}
+        <div id='skills'>
+            < Divider id='skills-divider-container' content={
+                <>
+                    <div id='skills-header'>
+                        <h1>SKILLS</h1>
+                        {['Languages', 'Frameworks', 'Tools'].map(type =>
+                            <span key={`${type}-span`}
+                                className={`skill-group ${selectedSkillType === type ? 'active' : ''}`}
+                                onClick={() => setSelectedSkillType(type)}>
+                                {type}
+                            </span>
+                        )}
                     </div>
-                    <div id='skills-divider'></div>
-                    <div id='skills-frameworks'>
-                        {frameworks.map((framework) => { return <div key={framework} className='skills-framework'>{framework}</div>; })}
-                    </div>
-                </div>
-            </>
-        } id='skills-divider-container' />
+                    {['Frameworks', 'Languages', 'Tools'].map(type => selectedSkillType === type && (
+                        <div key={type} className='skills-type' id={`${type}-skills-container`}>
+                            <div className='skills-container'>
+                                {skillsData.filter(skill => type.toLowerCase().includes(skill.type)).map(skill => (
+                                    <SkillCard key={skill.name} skill={skill} />
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </>
+            } />
+        </div>
     );
 }
 
-export default Skills;
+function SkillCard({ skill }: { skill: { name: string; type: string; description: string } }) {
+    return (
+        <div className='skill-card'>
+            <div className='card-front'>
+                <span>{skill.name}</span>
+            </div>
+            <div className='card-back'>
+                <span>{skill.description}</span>
+            </div>
+        </div>
+    );
+}
