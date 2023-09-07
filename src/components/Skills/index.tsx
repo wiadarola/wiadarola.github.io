390230100f255093c45324aee4f57d0b139d662c
William Iadarola <wiadarola0@gmail.com>
2023-09-06 21:32:15 -0700

    Added images and filled out skill cards

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -6,30 +6,32 @@ export default function Skills() {
     const [selectedSkillType, setSelectedSkillType] = useState('Languages');
 
     const skillsData = [
-        { name: 'JavaScript', type: 'language', description: 'Used in several frontend projects.' },
-        { name: 'TypeScript', type: 'language', description: 'Applied in React and Angular projects.' },
-        { name: 'HTML', type: 'language', description: 'Markup language for web development.' },
-        { name: 'CSS', type: 'language', description: 'Styling for web applications.' },
-        { name: 'C', type: 'language', description: 'Low-level programming for system development.' },
-        { name: 'C++', type: 'language', description: 'Object-oriented, used in various applications.' },
-        { name: 'Assembly', type: 'language', description: 'Lowest level human-readable language.' },
-        { name: 'Python', type: 'language', description: 'Versatile language for web, data science, and more.' },
-        { name: 'Java', type: 'language', description: 'Object-oriented, used in various applications.' },
-        { name: 'PHP', type: 'language', description: 'Server-side scripting for web development.' },
-        { name: 'SQL', type: 'language', description: 'Used for database queries.' },
-        { name: 'Flask', type: 'framework', description: 'Python micro web framework.' },
-        { name: 'React', type: 'framework', description: 'JavaScript library for building user interfaces.' },
-        { name: 'JQuery', type: 'framework', description: 'JavaScript library for DOM manipulation.' },
-        { name: 'MySQL', type: 'framework', description: 'Open-source relational database management system.' },
-        { name: 'Bootstrap', type: 'framework', description: 'CSS framework for responsive design.' },
-        { name: 'Oracle Database', type: 'framework', description: 'Object-relational database management system.' },
-        { name: 'Node.js', type: 'tool', description: 'Runtime for executing JavaScript on the server side.' },
-        { name: 'Vue.js', type: 'framework', description: 'Progressive framework for building user interfaces.' },
-        { name: 'Docker', type: 'tool', description: 'Platform for containerization.' },
-        { name: 'Kubernetes', type: 'tool', description: 'Container orchestration system.' },
-        { name: 'Git', type: 'tool', description: 'Version control system.' },
-        { name: 'PostgreSQL', type: 'framework', description: 'Open-source relational database.' }
+        { name: 'JavaScript', type: 'language', description: 'Scripting language for web interactivity.', link: '/src/assets/javascript.svg' },
+        { name: 'TypeScript', type: 'language', description: 'Superset of JS with static typing features.', link: '/src/assets/typescript.svg' },
+        { name: 'HTML', type: 'language', description: 'Markup language for web development.', link: '/src/assets/html5.svg' },
+        { name: 'CSS', type: 'language', description: 'Styling for web applications.', link: '/src/assets/css3.svg' },
+        { name: 'C', type: 'language', description: 'Low-level programming for system development.', link: '/src/assets/C_Programming_Language.svg' },
+        { name: 'C++', type: 'language', description: 'Object-oriented, used in various applications.', link: '/src/assets/c.svg' },
+        // { name: 'Assembly', type: 'language', description: 'Lowest level human-readable language.', link: '' },
+        { name: 'Python', type: 'language', description: 'Versatile language for web, data science, and more.', link: '/src/assets/python.svg' },
+        { name: 'Java', type: 'language', description: 'Object-oriented, used in various applications.', link: '/src/assets/java.svg' },
+        { name: 'PHP', type: 'language', description: 'Server-side scripting for web development.', link: '/src/assets/new-php-logo.svg' },
+        { name: 'SQL', type: 'language', description: 'Used for database queries.', link: '/src/assets/sql-database-generic.svg' },
+        { name: 'Flask', type: 'framework', description: 'Python micro web framework.', link: '/src/assets/pocoo_flask-icon.svg' },
+        { name: 'React', type: 'framework', description: 'JavaScript library for building user interfaces.', link: '/src/assets/react.svg' },
+        { name: 'JQuery', type: 'framework', description: 'JavaScript library for DOM manipulation.', link: '/src/assets/jquery-icon.svg' },
+        { name: 'MySQL', type: 'framework', description: 'Open-source relational database management system.', link: '/src/assets/mysql-icon.svg' },
+        { name: 'Bootstrap', type: 'framework', description: 'CSS framework for responsive design.', link: '/src/assets/bootstrap.svg' },
+        { name: 'Oracle Database', type: 'framework', description: 'Object-relational database management system.', link: '/src/assets/oracle-icon.svg' },
+        { name: 'Node.js', type: 'tool', description: 'Runtime for executing JavaScript on the server side.', link: '/src/assets/nodejs.svg' },
+        { name: 'Vue.js', type: 'framework', description: 'Progressive framework for building user interfaces.', link: '/src/assets/vuejs.svg' },
+        { name: 'Docker', type: 'tool', description: 'Platform for containerization.', link: '/src/assets/docker-tile.svg' },
+        { name: 'Kubernetes', type: 'tool', description: 'Container orchestration system.', link: '/src/assets/kubernetes-icon.svg' },
+        { name: 'Git', type: 'tool', description: 'Version control system.', link: '/src/assets/gitlab.svg' },
+        { name: 'PostgreSQL', type: 'framework', description: 'Open-source relational database.', link: '/src/assets/postgresql.svg' },
+        { name: 'Jira', type: 'tool', description: 'Project management tool.', link: '/src/assets/jira.svg' },
     ];
+
 
     return (
         <div id='skills'>
@@ -49,7 +51,9 @@ export default function Skills() {
                         <div key={type} className='skills-type' id={`${type}-skills-container`}>
                             <div className='skills-container'>
                                 {skillsData.filter(skill => type.toLowerCase().includes(skill.type)).map(skill => (
-                                    <SkillCard key={skill.name} skill={skill} />
+                                    <>
+                                        <SkillCard key={skill.name} skill={skill} />
+                                    </>
                                 ))}
                             </div>
                         </div>
@@ -60,15 +64,18 @@ export default function Skills() {
     );
 }
 
-function SkillCard({ skill }: { skill: { name: string; type: string; description: string } }) {
+const needBackground = new Set().add('/src/assets/java.svg').add('/src/assets/nodejs.svg').add('/src/assets/mysql-icon.svg');
+function SkillCard({ skill }: { skill: { name: string; type: string; description: string; link: string } }) {
     return (
         <div className='skill-card'>
             <div className='card-front'>
-                <span>{skill.name}</span>
+                <img src={skill.link} alt={`${skill.name} icon`} className={`skill-icon ${needBackground.has(skill.link) ? 'background' : ''}`} />
+                <span className='card-back-name'>{skill.name}</span>
             </div>
             <div className='card-back'>
-                <span>{skill.description}</span>
+                <span className='card-back-desc'>{skill.description}</span>
             </div>
         </div>
     );
 }
+
