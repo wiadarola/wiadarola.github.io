6fb6776dd5f699796971ee8223357a753a85668d
William Iadarola <wiadarola0@gmail.com>
2023-09-06 22:00:03 -0700

    Fixed svg link issue by adding prod config in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   plugins: [react()],
 })
