b920174605fa66c7bea15b561eef1efbae647411
William Iadarola <wiadarola0@gmail.com>
2023-09-06 17:20:00 -0700

    Implemented nav & hero width rules

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import './App.css';
 import NavBar from './components/NavBar';
 import NavTop from './components/NavTop';
 import Hero from './components/Hero';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Description from './components/Description';
-import About from './components/About';
-import Contact from './components/Contact';
+// import Skills from './components/Skills';
+// import Projects from './components/Projects';
+// import Description from './components/Description';
+// import About from './components/About';
+// import Contact from './components/Contact';
 
 function App() {
   return (
@@ -14,11 +14,11 @@ function App() {
       < NavBar />
       < NavTop />
       < Hero />
-      < Skills />
+      {/* < Skills />
       < Projects />
       < Description />
       < About />
-      < Contact />
+      < Contact /> */}
     </>
   );
 }
