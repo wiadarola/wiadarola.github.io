59e44ef3cf67e93ba37497b8ec347f04a3fc971f
William Iadarola <wiadarola0@gmail.com>
2023-09-09 14:17:50 -0700

    Added 2 project images & started formatting layout

--- src/components/ProjectBox/index.tsx
+++ src/components/ProjectBox/index.tsx
@@ -5,19 +5,19 @@ interface ProjectBoxProps {
     description: string;
     image: string;
     link: string;
-    linkText: string;
 }
 
 function ProjectBox(props: ProjectBoxProps) {
+    function handleClick() {
+        window.open(props.link, "_blank");
+    }
+
     return (
-        <div className="project-box">
-            <div className="project-box-image">
-                <img src={props.image} alt={props.title} />
-            </div>
-            <div className="project-box-text">
+        <div onClick={handleClick} className="project-box" style={{ backgroundImage: `url(${props.image})` }}>
+            <div className="project-box-content">
                 <h3>{props.title}</h3>
                 <p>{props.description}</p>
-                <a href={props.link} target="_blank" rel="noreferrer">{props.linkText}</a>
+                <a href={props.link} target="_blank" rel="noreferrer">LEARN MORE</a>
             </div>
         </div>
     );
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -5,34 +5,39 @@ import ProjectBox from "../ProjectBox";
 function Projects() {
     return (
         <section id="portfolio" className="page-section">
-            <div id="portfolio-header">
+            <div className="portfolio-header">
                 <h1 className="header">PROJECTS</h1>
                 < LinkButton href="https://github.com/wiadarola/" text="GitHub" newTab={true} />
             </div>
-            <div id="portfolio-content">
-                <ProjectBox
-                    title="Project 1"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl."
-                    image="https://via.placeholder.com/300"
-                    link="
-                    https://www.google.com/"
-                    linkText="Google"
-                />
-                <ProjectBox
-                    title="Project 2"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl."
-                    image="https://via.placeholder.com/300"
-                    link="https://www.google.com/"
-                    linkText="Google"
-                />
-
-                <ProjectBox
-                    title="Project 3"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl."
-                    image="https://via.placeholder.com/300"
-                    link="https://www.google.com/"
-                    linkText="Google"
-                />
+            <div className="portfolio-container">
+                <div className="portfolio-row1">
+                    <ProjectBox
+                        title="Project 1"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl."
+                        image="/assets/projectimg-music.jpg"
+                        link=" https://www.google.com/"
+                    />
+                    <ProjectBox
+                        title="Project 2"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl."
+                        image="/assets/projectimg-notes.jpg"
+                        link="https://www.google.com/"
+                    />
+                </div>
+                <div className="portfolio-row2">
+                    <ProjectBox
+                        title="Project 3"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl."
+                        image="https://via.placeholder.com/300"
+                        link="https://www.google.com/"
+                    />
+                    <ProjectBox
+                        title="Project 3"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nunc vitae nisl."
+                        image="https://via.placeholder.com/300"
+                        link="https://www.google.com/"
+                    />
+                </div>
             </div>
         </section>
     );
