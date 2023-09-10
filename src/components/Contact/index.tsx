57a1f128ffd55bd7734ce2234b360eb815f726a2
William Iadarola <wiadarola0@gmail.com>
2023-09-10 12:35:34 -0700

    Filled out rudimentary contacts section

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -3,18 +3,35 @@ import './index.css';
 function Contact() {
     return (
         <section id="contact" className='page-section'>
-            <header className='header'>CONTACT</header>
-            <span>Interested in working together? Let's connect.</span>
-            <div className='contact-container'>
-                <div className='contact-info'>
-                    {/* ToDo */}
-                </div>
-                <div className='contact-form'>
-                    {/* ToDo */}
-                </div>
+            <div>
+                <header className='header'>CONTACT</header>
             </div>
-            <button>SEND</button>
-        </section>
+            <div className='contact-container'>
+                <div>
+                    <div>
+                        <span><span className='contact-tag'>Interested in working together?</span><br />Let's connect.</span>
+                        <div className='contact-img-container'>
+                            <a href="https://www.linkedin.com/in/wiadarola/" target="_blank" rel="noopener noreferrer">
+                                <img className="contact-img" src="https://img.icons8.com/ios-filled/50/ffffff/linkedin.png" alt="LinkedIn" />
+                            </a>
+
+                            <a href="mailto:wiadarola0@gmail.com">
+                                <img className="contact-img" src="https://img.icons8.com/ios-filled/50/ffffff/mail.png" alt="Mail" />
+                            </a>
+                        </div>
+                    </div>
+                    <form className='contact-form'>
+                        <div>
+                            <input type="text" placeholder='Name' className='form-input' />
+                            <input type="text" placeholder='Email' className='form-input' />
+                            <input type="text" placeholder='Subject' className='form-input' />
+                        </div>
+                        <textarea placeholder='Message' className='form-txt'></textarea>
+                    </form>
+                    <button>SEND</button>
+                </div>
+            </div >
+        </section >
     );
 }
 
