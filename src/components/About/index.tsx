2c8f19dd10472eac0bc961721879f440158b594d
William Iadarola <wiadarola0@gmail.com>
2023-09-10 12:38:17 -0700

    minor styling changes

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -9,9 +9,9 @@ function About() {
                 <LinkButton href="https://www.linkedin.com/in/wiadarola/" text="LinkedIn" newTab={true} />
             </div>
             <div>
-                <span>Born in Southern California, I discovered my passion for programming in my senior year of highschool after joining the <span className="highlight">FIRST robotics team.</span></span>
-                <span>At <span className="highlight">New York University</span>, I honed my skills in Computer Science and fell for the city's allure and the intricacies of software engineering.</span>
-                <span>Outside coding, <span className="highlight">I enjoy staying active</span>,&nbsp;
+                <span>Born in Southern California, I <span className="highlight">discovered my passion</span> for programming in my senior year of highschool after joining the <span className="highlight">FIRST robotics team.</span></span>
+                <span>At <span className="highlight">New York University</span>, I honed my skills in <span className="highlight">Computer Science</span> and fell for the city's allure and the intricacies of software engineering.</span>
+                <span>Outside coding, I enjoy <span className="highlight">staying active</span>,&nbsp;
                     <span className="highlight">attending jazz and comedy nights</span>, and&nbsp;
                     <span className="highlight">cherishing the many parks the city has to offer</span>.</span>
             </div>
