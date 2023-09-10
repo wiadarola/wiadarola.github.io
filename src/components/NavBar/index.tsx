d5104ac2bc3193335b89da05bb664bfef6f04c2b
William Iadarola <wiadarola0@gmail.com>
2023-09-09 17:28:26 -0700

    Navbar hides when mouse drops below 1/4 of the screen

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,13 +1,31 @@
 import './index.css';
 import LinkButton from '../LinkButton';
+import { useEffect, useState } from 'react';
 
 /**
  * Renders a navigation bar component with a title and a button to go to the contact page.
  * @returns JSX.Element
  */
 function NavBar() {
+    const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        const handleMouseMove = (event: MouseEvent) => {
+            setMousePos({ x: event.clientX, y: event.clientY });
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener(
+                'mousemove',
+                handleMouseMove
+            );
+        };
+    }, []);
+
     return (
-        <section id="navbar">
+        <section id="navbar" className={`nav ${mousePos.y > (1 / 4 * window.innerHeight) ? 'hidden' : ''}`}>
             <div id='nav-bar'></div>
             <label id='nav-tag'>WJI.</label>
             <LinkButton href="#contact" text="Contact" newTab={false} />
