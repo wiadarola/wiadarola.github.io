d0e7375dfbff1c977f9588a2321f8796cc648106
William Iadarola <wiadarola0@gmail.com>
2023-09-09 17:29:28 -0700

    Rewrote logic for better elasticity for proejcts

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,45 +1,67 @@
 import "./index.css";
 import LinkButton from "../LinkButton";
-import ProjectBox from "../ProjectBox";
 
 function Projects() {
     return (
-        <section id="portfolio" className="page-section">
-            <div className="portfolio-header">
-                <h1 className="header">PROJECTS</h1>
+        <section className="projects">
+            <div className="projects-header">
+                <header className="projects-title">PROJECTS</header>
                 < LinkButton href="https://github.com/wiadarola/" text="GitHub" newTab={true} />
             </div>
-            <div className="portfolio-container">
-                <div className="portfolio-row1">
-                    <ProjectBox
-                        title="Spotify Clone"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies."
-                        image="/assets/projectimg-music.jpg"
-                        link="https://wiadarola.github.io/spotify-clone/"
-                    />
+            <div className="projects-content">
+                <div>
                     <ProjectBox
                         title="React ToDo App"
                         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies."
                         image="/assets/projectimg-notes.jpg"
                         link="https://wiadarola.github.io/react-ts-todo/"
                     />
-                </div>
-                <div className="portfolio-row2">
                     <ProjectBox
-                        title="MTG Price Predictor"
+                        title="Spotify Clone"
                         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies."
-                        image="/assets/projectimg-stock.webp"
-                        link=" https://github.com/wiadarola/mtg-price-predictor"
+                        image="/assets/projectimg-music.jpg"
+                        link="https://wiadarola.github.io/spotify-clone/"
                     />
+                </div>
+                <div>
                     <ProjectBox
                         title="CNN Depth Estimator"
                         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies."
                         image="/assets/projectimg-bino.jpeg"
                         link="https://github.com/wiadarola/CV-Monocular-Depth-Estimator"
                     />
+                    <ProjectBox
+                        title="MTG Price Predictor"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies."
+                        image="/assets/projectimg-stock.webp"
+                        link=" https://github.com/wiadarola/mtg-price-predictor"
+                    />
                 </div>
             </div>
         </section>
+    );
+}
+
+interface ProjectBoxProps {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+}
+
+function ProjectBox(props: ProjectBoxProps) {
+    function handleClick() {
+        window.open(props.link, "_blank");
+    }
+
+    return (
+        <div onClick={handleClick} className="project-box" style={{ backgroundImage: `url(${props.image})` }}>
+            <div className="project-box-content">
+                <header>{props.title}</header>
+                <p>{props.description}</p>
+                <a href={props.link} target="_blank" rel="noreferrer">LEARN MORE</a>
+            </div>
+        </div>
     );
 }
 
