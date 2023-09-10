fe25ade2db992a5e3c3d7b8eefcbca52b128a559
William Iadarola <wiadarola0@gmail.com>
2023-09-10 12:41:35 -0700

    Footer fits in better with overall theme

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,10 +3,10 @@ import './index.css';
 export default function Footer() {
     return <footer>
         <span>Built with React, TypeScript, HTML, & CSS</span>
-        <a href="#hero">Top</a>
-        <a href="#skills">Skills</a>
-        <a href="#projects">Projects</a>
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a>
+        <a href="#hero">TOP</a>
+        <a href="#skills">SKILLS</a>
+        <a href="#projects">PROJECTS</a>
+        <a href="#about">ABOUT</a>
+        <a href="#contact">CONTACT</a>
     </footer>
 }
