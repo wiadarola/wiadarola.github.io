f4c16e525839374882a3851de1bb679f52ab7d2c
William Iadarola <wiadarola0@gmail.com>
2023-09-12 19:29:03 -0700

    Flexible contact layout

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -10,7 +10,7 @@ function Contact() {
             <div className='contact-container'>
                 <div>
                     <span className='contact-tag'>Interested in working together?<LinkButton href="https://www.linkedin.com/in/wiadarola/" text="Let's connect on LinkedIn" newTab={true} /></span>
-                    <span className='contact-tag'>Want to see my resume?<LinkButton href="/assets/resume.pdf" text="Check it out" newTab={true} /></span>
+                    <span className='contact-tag'>Want to see my resume?<LinkButton href="/assets/resume.pdf" text="Take a look" newTab={true} /></span>
                     <span className='contact-tag'>Want to see more?<LinkButton href="https://github.com/wiadarola/" text="Check out my GitHub" newTab={true} /></span>
                     <span className='contact-tag'>Have a question?<LinkButton href="mailto:wiadarola0@gmail.com" text="Shoot me an Email" newTab={true} /></span>
                 </div >
