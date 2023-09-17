68603aa360070915d16c2f49e8ff01b3cbc1865c
William Iadarola <wiadarola0@gmail.com>
2023-09-17 10:19:01 -0700

    Revised contact btns

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -9,10 +9,10 @@ function Contact() {
             </div>
             <div className='contact-container'>
                 <div>
-                    <span className='contact-tag'>Interested in working together?<LinkButton href="https://www.linkedin.com/in/wiadarola/" text="Let's connect on LinkedIn" newTab={true} /></span>
+                    <span className='contact-tag'>Interested in working together?<LinkButton href="https://www.linkedin.com/in/wiadarola/" text="Let's Connect" newTab={true} /></span>
                     <span className='contact-tag'>Want to see my resume?<LinkButton href="/assets/resume.pdf" text="Take a look" newTab={true} /></span>
-                    <span className='contact-tag'>Want to see more?<LinkButton href="https://github.com/wiadarola/" text="Check out my GitHub" newTab={true} /></span>
-                    <span className='contact-tag'>Have a question?<LinkButton href="mailto:wiadarola0@gmail.com" text="Shoot me an Email" newTab={true} /></span>
+                    <span className='contact-tag'>Want to see more?<LinkButton href="https://github.com/wiadarola/" text="Visit my GitHub" newTab={true} /></span>
+                    <span className='contact-tag'>Have a question?<LinkButton href="mailto:wiadarola0@gmail.com" text="Send Me an Email" newTab={true} /></span>
                 </div >
                 <span>Thanks for visiting my portfolio</span >
             </div >
